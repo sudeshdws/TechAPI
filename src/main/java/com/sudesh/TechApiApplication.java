497547689257fa66e4786923e2339a76bb60c2a4
Sudesh <sudeshdws@gmail.com>
2018-09-09 16:42:30 +0530

    Initial Commt

---
+++ src/main/java/com/sudesh/TechApiApplication.java
@@ -0,0 +1,12 @@
+package com.sudesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechApiApplication.class, args);
+	}
+}
